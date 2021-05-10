efccb141045e500f706cb7944c43f7526991dbdb
MartinKurej <55577253+MartinKurej@users.noreply.github.com>
2021-05-10 14:28:20 +0200

    Update SumSumsOfEvenAndMultipleOfOdd.java
    remade

--- src/main/java/cz/educanet/praha/SumSumsOfEvenAndMultipleOfOdd.java
+++ src/main/java/cz/educanet/praha/SumSumsOfEvenAndMultipleOfOdd.java
@@ -5,7 +5,17 @@ import java.util.Scanner;
 public class SumSumsOfEvenAndMultipleOfOdd {
 
     public static int sumSumsOfEvenAndMultipleOfOdd(Scanner scanner) {
-        return 0;
+        int i = 0;
+        int y = 1;
+        while(scanner.hasNext()){
+            int x = scanner.nextInt();
+            if (x%2 == 0){
+                i = i + x;
+            }else {
+               y = y * x;
+            }
+        }
+        return i + y;
     }
 
     public static void main(String[] args) {
