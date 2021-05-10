e5fe4587b951fd76c390fad62765abc73ca4b3e0
MartinKurej <55577253+MartinKurej@users.noreply.github.com>
2021-05-10 14:23:41 +0200

    rework

--- src/main/java/cz/educanet/praha/SumAllNumbers.java
+++ src/main/java/cz/educanet/praha/SumAllNumbers.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 public class SumAllNumbers {
 
     public static int calculate(Scanner scanner) {
-        return 0;
+        int i = 0;
+
+        while(scanner.hasNext()){
+            int y = scanner.nextInt();
+            i = i + y;
+        }
+
+        return i;
     }
 
     public static void main(String[] args) {
