7e0448e4b42d10e42f0cbb7bc7e1143c2a8f64de
MartinKurej <55577253+MartinKurej@users.noreply.github.com>
2021-06-11 11:04:53 +0200

    Update StringWithMostOccurrencesOfLetter.java

--- src/main/java/cz/educanet/praha/StringWithMostOccurrencesOfLetter.java
+++ src/main/java/cz/educanet/praha/StringWithMostOccurrencesOfLetter.java
@@ -3,6 +3,21 @@ package cz.educanet.praha;
 public class StringWithMostOccurrencesOfLetter {
 
     public static String findStringWithMostOccurrencesOfLetter(String[] strings, char letter) {
-        return "";
+        int nejPoc = 0;
+        String nejString = null;
+        for(int i = 0; i < strings.length;i++){
+            String string = strings[i];
+            int pocet = 0;
+            for (int x = 0; x < string.length(); x++) {
+                char y = string.charAt(x);
+                if(y == letter){
+                    pocet++;
+                }
+            }if(pocet > nejPoc ){
+                nejPoc = pocet;
+                nejString = string;
+            }
+        }
+        return nejString;
     }
 }
